6dc5a50ebd95234f0efcf93a49ead2092935dcc5
Liu <gentle9278@gmail.com>
2021-09-22 11:21:43 +0800

    HomePage remove header and footer components

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,9 +1,7 @@
 import styled from 'styled-components'
-import Header from '../.././components/Header/Header.js'
 import Banner from './components/Banner.js'
 import Characteristic from './components/Characteristic.js'
 import HotSales from './components/HotSales.js'
-import Footer from '../.././components/Footer/Footer.js'
 
 const Wrapper = styled.div`
   max-width: 1024px;
@@ -12,13 +10,11 @@ const Wrapper = styled.div`
 function HomePage() {
   return (
     <div>
-      <Header/>
       <Banner/>
       <Wrapper>
         <Characteristic/>
         <HotSales/>
       </Wrapper>
-      <Footer/>
     </div>
   );
 }
