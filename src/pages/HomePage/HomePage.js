4c7e3d1deb52973b242d79bf1764b21a2632e9dc
Liu <gentle9278@gmail.com>
2021-09-22 10:56:42 +0800

    HomeIndex Component HotSales and Characteristic

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,3 +1,26 @@
-export default function HomePage() {
-  return 123
+import styled from 'styled-components'
+import Header from '../.././components/Header/Header.js'
+import Banner from './components/Banner.js'
+import Characteristic from './components/Characteristic.js'
+import HotSales from './components/HotSales.js'
+import Footer from '../.././components/Footer/Footer.js'
+
+const Wrapper = styled.div`
+  max-width: 1024px;
+  margin: 0 auto;
+`
+function HomePage() {
+  return (
+    <div>
+      <Header/>
+      <Banner/>
+      <Wrapper>
+        <Characteristic/>
+        <HotSales/>
+      </Wrapper>
+      <Footer/>
+    </div>
+  );
 }
+
+export default HomePage;
