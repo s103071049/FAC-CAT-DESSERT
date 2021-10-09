aeb603f0e21278df042d74c6bc8f0d7af3e234c5
Ashi <backas36@gmail.com>
2021-10-09 12:59:41 +0800

    user page rwd layout

--- src/pages/UserPage/UserPage.js
+++ src/pages/UserPage/UserPage.js
@@ -8,9 +8,8 @@ import {
 } from 'react-router-dom'
 import IconMark from "../../components/contexts/IconMark"
 import UserInfo from "./components/UserInfo"
-import User from './components/User'
 import EditPassword from "./components/EditPassword"
-import { MEDIA_QUERY_SD } from "../../components/Style/style"
+import { MEDIA_QUERY_MD } from "../../components/Style/style"
 
 
 const Wrapper = styled.div`
@@ -25,37 +24,45 @@ const UserWrapper = styled.div`
   padding:0 16px;
   display:flex;
 
-  ${MEDIA_QUERY_SD} {
+  ${MEDIA_QUERY_MD} {
     flex-direction:column;
-    align-items:center;
+    padding:0;
+    margin:0 auto;
+    justify-content:space-between;
   }
 
 `
 const SideBar = styled.div`
   padding:12px;
   width:250px;
-  ${MEDIA_QUERY_SD} {
-   margin-bottom:40px;
+  
+  ${MEDIA_QUERY_MD} {
+   padding:0;
+   width:100%;
+   box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
   }
 `
-const Main = styled.div`
-  flex-grow:1;
-   ${MEDIA_QUERY_SD} {
-    border-top:2px solid #E2E2E2;
-    padding-top:40px;
-  }
-`
+
 const SideBarHeader = styled.div`
   background:#FBF6EA;
   padding:12px 24px;
   text-align:center;
   color:#424242;
+
+  ${MEDIA_QUERY_MD} {
+    display:none;
+  }
 `
 
 const SideBarbody = styled.div`
   margin-top:20px;
   display:flex;
   flex-direction:column;
+
+  ${MEDIA_QUERY_MD} {
+     flex-direction:row;
+     margin:0;
+  }
 `
 const SideBarItem = styled(Link)`
   display:block;
@@ -64,7 +71,6 @@ const SideBarItem = styled(Link)`
   padding:20px;
   border-bottom: 1px solid #808080;
   position:relative;
-
   &:hover {
     color:#D49E6A;
   }
@@ -74,17 +80,46 @@ const SideBarItem = styled(Link)`
     font-weight:700;
   `}
 
-  &::after {
+  &:not(:last-child)::after {
     content:">";
     position: absolute;
     top: 30%;
     right: 0px;
   }
+
+  ${MEDIA_QUERY_MD} {
+    width:25%;
+    padding:20px;
+    border-bottom: 0;
+    text-align:center;
+    &:not(:last-child)::after {
+      content:"";
+    }
+  }
+`
+const Main = styled.div`
+  flex-grow:1;
+   ${MEDIA_QUERY_MD} {
+    border-top:2px solid #E2E2E2;
+    padding-top:40px;
+  }
 `
 const UserPage = () => {
   let location = useLocation()
-
   let { path, url } = useRouteMatch()
+
+  const SideBarItems = ({ toUrl, title }) => {
+    let newUrl = url + toUrl
+    return (
+      <SideBarItem
+        to={`${newUrl}`}
+        $active={location.pathname === `${newUrl}`}
+      >
+        {title}
+      </SideBarItem>
+    )
+  }
+
   return (
     <>
       <Wrapper>
@@ -93,33 +128,16 @@ const UserPage = () => {
           <SideBar>
             <SideBarHeader>我的帳戶</SideBarHeader>
             <SideBarbody>
+              <SideBarItems toUrl={''} title='更新個人資訊' />
+              <SideBarItems toUrl={'/editPWD'} title='更改密碼' />
+              <SideBarItems toUrl={'/myorders'} title='我的訂單' />
+              <SideBarItems toUrl={'/logout'} title='登出' />
 
-              <SideBarItem
-                to={`${url}/info`}
-                $active={location.pathname === `${url}/info`}
-              >
-                更新個人資訊
-              </SideBarItem>
-              <SideBarItem
-                to={`${url}/editPWD`}
-                $active={location.pathname === `${url}/editPWD`}
-              >
-                更改密碼
-              </SideBarItem>
-              <SideBarItem
-                to={`${url}/myorders`}
-                $active={location.pathname === `${url}/myorders`}
-              >
-                我的訂單
-              </SideBarItem>
             </SideBarbody>
           </SideBar>
           <Switch>
             <Main>
               <Route exact path={path}>
-                <User />
-              </Route>
-              <Route path={`${path}/info`}>
                 <UserInfo />
               </Route>
               <Route path={`${path}/editPWD`}>
