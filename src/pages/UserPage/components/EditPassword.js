7237c392f986ab0118d603db975fe4fbb537a326
Ashi <backas36@gmail.com>
2021-10-09 14:18:59 +0800

    refator & fix layout

--- src/pages/UserPage/components/EditPassword.js
+++ src/pages/UserPage/components/EditPassword.js
@@ -1,89 +1,7 @@
 import { useState } from "react/cjs/react.development"
-import styled from "styled-components"
-import { MEDIA_QUERY_SD } from "../../../components/Style/style"
+import * as UFS from './UserLayout/UserFormStyle'
 
-const FormWrapper = styled.div`
-  margin:0 auto;
-  padding-top:80px;
-  padding-left:20px;
-  & div {
-    margin-bottom:20px;
-  }
-
-  ${MEDIA_QUERY_SD} {
-    padding-top:0;
-    padding-left:0;
-
-  }
- 
-`
-const FormRow = styled.div`
-  &:last-child {
-    margin-top:50px;
-  }
-
-  ${MEDIA_QUERY_SD} {
-    &:last-child {
-      text-align:center;
-    }
-  }
-`
-const FormItem = styled.div`
-  padding: 0 20px;
-  
-  & label {
-    color:#212529;
-    font-size:16px;
-    display:block;
-  }
-
-  & input {
-    margin-top:10px;
-    width:100%;
-    border:1px solid #CED4DA;
-    padding:10px 20px;
-    border-radius:6px;
-  }
-
-   
-  
-`
-const WarnningText = styled.p`
-  padding: 0 20px;
-  color:#E55555;
-  margin-bottom:50px;
-`
-const Button = styled.button`
-  color : #ABB4BB;
-  border:1px solid #ABB4BB;
-  background:#fff;
-  border-radius:6px;
-  padding:6px 60px;
-  cursor:pointer;
-  transition:all 0.1s;
-
-  & + & {
-    margin-left:30px;
-  }
- 
-  &[type="submit"] {
-    color:#D49E6A;
-    border:1px solid #D49E6A;
-  }
-
-  &:hover {
-    background:#D49E6A;
-    color:white;
-  }
-
-   ${MEDIA_QUERY_SD} {
-    & + & {
-      margin:0;
-      margin-top:30px;
-    }
-  }
-
-`
+import UserActionBtn from "./UserLayout/UserActionBtn"
 
 const EditPassword = () => {
   const [oldPassword, setOldPassword] = useState('')
@@ -91,42 +9,39 @@ const EditPassword = () => {
   const [newPassword2, setNewPassword2] = useState('')
 
   return (
-    <FormWrapper>
-      <FormRow>
-        <FormItem>
+    <UFS.FormWrapper>
+      <UFS.FormRow>
+        <UFS.FormItem>
           <label htmlFor="oldPassword">
             目前的密碼
           </label>
           <input value={oldPassword} id="oldPassword" onChange={(e) => setOldPassword(e.target.value)} type="password" />
 
-        </FormItem>
-      </FormRow>
-      <FormRow>
-        <FormItem>
+        </UFS.FormItem>
+      </UFS.FormRow>
+      <UFS.WarnningText>*請輸入6個字元以上的英文字母及數字，不可使用特殊符號</UFS.WarnningText>
+      <UFS.FormRow>
+        <UFS.FormItem>
           <label htmlFor="newPassword">
             新的密碼
           </label>
           <input value={newPassword} id="newPassword" onChange={(e) => setNewPassword(e.target.value)} type="password" />
-        </FormItem>
-        <WarnningText>*請輸入6個字元以上的英文字母及數字，不可使用特殊符號</WarnningText>
-      </FormRow>
-      <FormRow>
-        <FormItem>
+        </UFS.FormItem>
+      </UFS.FormRow>
+      <UFS.FormRow>
+        <UFS.FormItem>
           <label htmlFor="newPassword2">
             新密碼確認
           </label>
           <input value={newPassword2} id="newPassword2" onChange={(e) => setNewPassword2(e.target.value)} type="password" />
-        </FormItem>
-      </FormRow>
-      <FormRow>
-        <FormItem>
-          <Button>重新填寫</Button>
-          <Button type="submit">確定修改</Button>
-        </FormItem>
-      </FormRow>
+        </UFS.FormItem>
+      </UFS.FormRow>
+      <UFS.FormRow>
+        <UserActionBtn />
+      </UFS.FormRow>
 
 
-    </FormWrapper>
+    </UFS.FormWrapper>
   )
 }
 
--- src/pages/UserPage/components/UserInfo.js
+++ src/pages/UserPage/components/UserInfo.js
@@ -1,88 +1,6 @@
 import { useState } from "react/cjs/react.development"
-import styled from "styled-components"
-import { MEDIA_QUERY_SD } from "../../../components/Style/style"
-
-const FormWrapper = styled.div`
-  margin:0 auto;
-  display:flex;
-  flex-direction:column;
-  padding-top:80px;
-  padding-left:20px;
-  & div {
-    margin-bottom:20px;
-  }
-
-  ${MEDIA_QUERY_SD} {
-    padding-top:0;
-    padding-left:0;
-
-  }
- 
-`
-const FormRow = styled.div`
-  display:flex;
-  &:last-child {
-    margin-top:50px;
-  }
-
-  ${MEDIA_QUERY_SD} {
-    display:flex;
-    flex-direction:column;
-    &:last-child {
-      text-align:center;
-      margin-top:20px;
-
-    }
-  }
-`
-const FormItem = styled.div`
-  padding: 0 20px;
-  flex-grow:1;
-  & label {
-    color:#212529;
-    font-size:16px;
-    display:block;
-  }
-
-  & input {
-    margin-top:10px;
-    width:100%;
-    border:1px solid #CED4DA;
-    padding:10px 20px;
-    border-radius:6px;
-  }
-`
-const Button = styled.button`
-  color : #ABB4BB;
-  border:1px solid #ABB4BB;
-  background:#fff;
-  border-radius:6px;
-  padding:6px 36px;
-  cursor:pointer;
-  transition:all 0.1s;
-
-  & + & {
-    margin-left:8px;
-  }
- 
-  &[type="submit"] {
-    color:#D49E6A;
-    border:1px solid #D49E6A;
-  }
-
-  &:hover {
-    background:#D49E6A;
-    color:white;
-  }
-
-   ${MEDIA_QUERY_SD} {
-    & + & {
-      margin:0;
-      margin-top:20px;
-    }
-  }
-
-`
+import * as UFS from './UserLayout/UserFormStyle'
+import UserActionBtn from "./UserLayout/UserActionBtn"
 
 const UserInfo = () => {
   const [lastName, setLastName] = useState('Yang')
@@ -90,55 +8,52 @@ const UserInfo = () => {
   const [nickName, setNickName] = useState('yangyang')
   const [phone, setPhone] = useState('0912345678')
   return (
-    <FormWrapper>
-      <FormRow>
-        <FormItem>
+    <UFS.FormWrapper>
+      <UFS.FormRow>
+        <UFS.FormItem>
           <label htmlFor="lastName">
             姓
           </label>
           <input value={lastName} id="lastName" onChange={(e) => setLastName(e.target.value)} />
 
-        </FormItem>
-        <FormItem>
+        </UFS.FormItem>
+        <UFS.FormItem>
           <label htmlFor="firstName">
             名
           </label>
           <input value={firstName} id="firstName" onChange={(e) => setFirstName(e.target.value)} />
 
-        </FormItem>
-      </FormRow>
-      <FormRow>
-        <FormItem>
+        </UFS.FormItem>
+      </UFS.FormRow>
+      <UFS.FormRow>
+        <UFS.FormItem>
           <label htmlFor="nickName">
             暱稱
           </label>
           <input value={nickName} id="nickName" onChange={(e) => setNickName(e.target.value)} />
 
-        </FormItem>
-        <FormItem>
+        </UFS.FormItem>
+        <UFS.FormItem>
           <label htmlFor="phone">
             手機
           </label>
           <input value={phone} id="phone" onChange={(e) => setPhone(e.target.value)} />
 
-        </FormItem>
-      </FormRow>
-      <FormRow>
-        <FormItem>
+        </UFS.FormItem>
+      </UFS.FormRow>
+      <UFS.FormRow>
+        <UFS.FormItem>
           <label>
             Email
           </label>
           <input type="email" value={"123@gmail.com"} disabled="disabled" />
 
-        </FormItem>
-      </FormRow>
-      <FormRow>
-        <FormItem>
-          <Button>重新填寫</Button>
-          <Button type="submit">確定修改</Button>
-        </FormItem>
-      </FormRow>
-    </FormWrapper>
+        </UFS.FormItem>
+      </UFS.FormRow>
+      <UFS.FormRow>
+        <UserActionBtn />
+      </UFS.FormRow>
+    </UFS.FormWrapper>
   )
 }
 
