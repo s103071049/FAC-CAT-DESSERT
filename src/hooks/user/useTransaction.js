602d6639b91cd961c1895c5dfa5301c3fd2ea469
k1325699 <k1325699@gmail.com>
2021-11-03 12:52:16 +0800

    fix.user-orders 新增分頁，去除繳款狀態

--- src/hooks/user/useTransaction.js
+++ src/hooks/user/useTransaction.js
@@ -1,66 +1,62 @@
-import {useState, useEffect,useCallback, useContext} from 'react'
+import { useState, useEffect, useCallback, useContext } from "react";
 import { useHistory } from "react-router-dom";
 
-import {getHistory} from '../../WEBAPI'
-import { AuthLoadingContext } from '../../context'
+import { getHistory } from "../../WEBAPI";
+import { AuthLoadingContext } from "../../context";
 
 const useTransaction = () => {
   const transactionTableHeads = [
-    { id: 1, 
-      title: "訂單編號",
-    },
-    { id: 2, 
-      title: "總金額", 
-    },
-    { id: 3, 
-      title: "收件者", 
-    },
-    { id: 4, 
-      title: "交易日期",
-    },
-    { id: 5, 
-      title: "收貨日期",
-    },
-    { id: 6, 
-      title: "處理情形",
-    },
-    { id: 7, 
-      title: "繳款情形",
-    }
-  ]
-  const {setLoading} = useContext(AuthLoadingContext)
-  
-  const [transactions, setTransactions] = useState([])
+    { id: 1, title: "訂單編號" },
+    { id: 2, title: "總金額" },
+    { id: 3, title: "收件者" },
+    { id: 4, title: "交易日期" },
+    { id: 5, title: "收貨日期" },
+    { id: 6, title: "處理情形" },
+  ];
+  const { setLoading } = useContext(AuthLoadingContext);
+
+  const [transactions, setTransactions] = useState([]);
   const history = useHistory();
+  //初始分頁
+  const [num, setNum] = useState(0);
+  const [pagenum, setPageNum] = useState(1);
 
-  const fetchTransactions = useCallback(()=>{
-    const fetchingTransactions = async() => {
-      setLoading(true)
-      const result = await getHistory()
-      try{
-        if(!result.success) {
-          setLoading(false)
-          return history.goBack()
+  const fetchTransactions = useCallback(() => {
+    const fetchingTransactions = async () => {
+      setLoading(true);
+      setNum(0);
+      setPageNum(1);
+      const result = await getHistory();
+      try {
+        if (!result.success) {
+          setLoading(false);
+          return history.goBack();
         }
-        const getTransactions = result.data.sort((a,b) => Date.parse(b.updatedAt) - Date.parse(a.updatedAt))
-        setTransactions(getTransactions)
-        setLoading(false)
-      }catch(err){
-        setLoading(false)
-        return history.goBack()
+        const getTransactions = result.data.sort(
+          (a, b) => Date.parse(b.updatedAt) - Date.parse(a.updatedAt)
+        );
+        setTransactions(getTransactions);
+        setLoading(false);
+      } catch (err) {
+        setLoading(false);
+        return history.goBack();
       }
-    } 
-    fetchingTransactions()
-  },[history, setLoading])
+    };
+    fetchingTransactions();
+  }, [history, setLoading]);
 
-  useEffect(()=>{
-    fetchTransactions()
-  },[fetchTransactions])
+  useEffect(() => {
+    fetchTransactions();
+  }, [fetchTransactions]);
 
   return {
     transactionTableHeads,
-    transactions
-  }
-}
+    transactions,
+    num,
+    setNum,
+    pagenum,
+    setPageNum,
+  };
+};
 
-export default useTransaction
+export default useTransaction;
--- src/pages/TransactionPage/TransactionPage.js
+++ src/pages/TransactionPage/TransactionPage.js
@@ -4,15 +4,20 @@ import { Link } from "react-router-dom";
 
 import { MEDIA_QUERY_SD, MEDIA_QUERY_MD } from "../../components/Style/style";
 import useTransaction from "../../hooks/user/useTransaction";
+import usePagination from "../../hooks/common/usePagination";
+import PageBtn from "../../components/common/PageBtn";
 
 const Wrapper = styled.div`
-  padding-top: 80px;
-  padding-left: 20px;
-`
+  padding: 80px 0 15px 20px;
+  display: flex;
+  flex-direction: column;
+`;
 
 const Table = styled.table`
   border-collapse: collapse;
   border: 1px solid #bdc0ba;
+  margin-bottom: 60px;
+  text-align: center;
   ${MEDIA_QUERY_MD} {
     display: block;
     border: none;
@@ -29,16 +34,18 @@ const Tbody = styled.tbody`
   }
 `;
 const IDLink = styled(Link)`
-  color:#000;
-  &:hover{
-    color:#789C9A;
+  color: #000;
+  &:hover {
+    color: #789c9a;
   }
-`
+`;
 const Column = styled.td`
   border: 1px solid #bdc0ba;
   padding: 12px;
 
-  ${props => props.$is_accepted === false &&`
+  ${(props) =>
+    props.$is_accepted === false &&
+    `
     color:#E33333;
   `}
 
@@ -70,9 +77,6 @@ const Column = styled.td`
     &:nth-of-type(6):before {
       content: "處理情形";
     }
-    &:nth-of-type(7):before {
-      content: "繳款情形";
-    }
   }
   ${MEDIA_QUERY_SD} {
     white-space: pre-line;
@@ -92,8 +96,8 @@ const Row = styled.tr`
   ${MEDIA_QUERY_MD} {
     display: block;
     margin: 0 0 16px 0;
-    &:nth-child(odd){
-      background:#77969A;
+    &:nth-child(odd) {
+      background: #77969a;
     }
   }
 `;
@@ -104,73 +108,75 @@ const RowHeader = styled(Row)`
     left: -9999px;
   }
 `;
-const Page = styled.div`
-  margin-top:60px;
-`;
-
-
 const TransactionPage = () => {
   const {
     transactionTableHeads,
-    transactions 
-  } = useTransaction()
-
+    transactions,
+    num,
+    setNum,
+    pagenum,
+    setPageNum,
+  } = useTransaction();
+  const pageSize = 4;
+  const { pageDetail, pageNext } = usePagination(transactions, pageSize);
 
   function Head() {
     return (
       <Thead>
         <RowHeader>
-          { transactionTableHeads.map(head=>{
-            return <GrayColumn key={head.id}>{head.title}</GrayColumn>
-          }) }
+          {transactionTableHeads.map((head) => {
+            return <GrayColumn key={head.id}>{head.title}</GrayColumn>;
+          })}
         </RowHeader>
       </Thead>
-
-    )
+    );
   }
 
   function Body() {
-  
-    const listItems = transactions.map(transaction => {
-        const {id, price, receiverName, createdAt, deliverDate, is_accepted, is_completed} = transaction
+    const listItems = pageDetail.indexList.map((transaction) => {
+      const { id, price, receiverName, createdAt, deliverDate, is_accepted } =
+        transaction;
 
       return (
         <Row key={id}>
           <Column>
-            <IDLink to={`/user/myorder/${id}`}>{id.slice(-6)}
-            </IDLink>
+            <IDLink to={`/user/myorder/${id}`}>{id.slice(-6)}</IDLink>
           </Column>
           <Column>{price}</Column>
           <Column>{receiverName}</Column>
-          <Column>
-            {new Date(createdAt).toLocaleString()}</Column>
+          <Column>{new Date(createdAt).toLocaleString()}</Column>
           <Column>{new Date(deliverDate).toLocaleDateString()}</Column>
           <Column $is_accepted={is_accepted}>
-            {is_accepted ? "已接受":is_accepted === null? '待處理':'拒絕'}
-          
-          </Column>
-          <Column>
-            {is_completed? '已付款':'待付款'}
+            {is_accepted ? "已接受" : is_accepted === null ? "待處理" : "拒絕"}
           </Column>
         </Row>
-      )
+      );
     });
-    return (
-      <Tbody>
-        {listItems}
-      </Tbody>
-    );
+    return <Tbody>{listItems}</Tbody>;
   }
 
   return (
     <>
       <Wrapper>
-        {transactions.length ? (<Table>
-          <Head />
-          <Body />
-        </Table>):(<p>尚未有訂單</p>)}
-        <Page>
-        </Page>
+        {transactions.length ? (
+          <>
+            <Table>
+              <Head />
+              <Body />
+            </Table>
+            {/* 分頁元件 */}
+            <PageBtn
+              pageNext={pageNext}
+              pageDetail={pageDetail}
+              num={num}
+              setNum={setNum}
+              pagenum={pagenum}
+              setPageNum={setPageNum}
+            />
+          </>
+        ) : (
+          <p>尚未有訂單</p>
+        )}
       </Wrapper>
     </>
   );
